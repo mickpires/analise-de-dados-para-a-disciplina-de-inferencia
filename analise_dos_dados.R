753aeb03d605a55ca8ce8afabbf03b8ef313c8e5
mickpires <83048320+mickpires@users.noreply.github.com>
2023-11-30 23:54:33 -0300

    adicionado os dados de 2019

--- analise_dos_dados.R
+++ analise_dos_dados.R
@@ -1,9 +1,15 @@
+{
 library(tidyverse)
 library(ggplot2)
 library(fdth)
 library(dplyr)
+}
 
-dados <- read.csv('MICRODADOS_CADASTRO_CURSOS_2022.CSV',sep=';',encoding = 'latin1')
+dados2022 <- read.csv('MICRODADOS_CADASTRO_CURSOS_2022.CSV',sep=';',encoding = 'latin1')
 
-fisica <- dados %>% filter(NO_CURSO =='Física')
-view(fisica)
+fisica2022 <- dados2022 %>% filter(NO_CURSO =='Física' & TP_GRAU_ACADEMICO == 1)
+view(fisica2022)
+
+dados2019 <- read.csv('MICRODADOS_CADASTRO_CURSOS_2019.CSV',sep=';',encoding='latin1')
+fisica2019 <- dados2019 %>% filter(NO_CURSO == 'FÍSICA' & TP_GRAU_ACADEMICO == 1)
+view(fisica2019)
